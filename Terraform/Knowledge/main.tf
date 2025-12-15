9a9a48dcc63668f6b44428df0ecadaeb4a3bbe41
Vishnu-NVD <vishnu.muthusamy@servion.com>
2025-12-15 13:23:00 +0530

    Update main.tf

--- Terraform/Knowledge/main.tf
+++ Terraform/Knowledge/main.tf
@@ -1,15 +1,18 @@
-
 terraform {
-  cloud {
-    organization = "Cx_As_Code"
-    workspaces {
-      name = "Knowledge"
-    }
-  }
+cloud { 
+    
+    organization = "Cx_As_Code" 
+
+    workspaces { 
+      name = "Knowledge" 
+    } 
+  } 
   required_providers {
     genesyscloud = {
-      source  = "mypurecloud/genesyscloud"
-      version = "~> 1.70.0"   # keep your desired range, or pin exact
+      source  = "MyPureCloud/genesyscloud"
+      version = "~> 1.70.0"
     }
   }
-}
+ }
+
+provider "genesyscloud" {}
