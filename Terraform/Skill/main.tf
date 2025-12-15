2541ce8a952165383d2939e523adc813344465c2
Vishnu-NVD <vishnu.muthusamy@servion.com>
2025-12-15 11:47:42 +0530

    Update main.tf

--- Terraform/Skill/main.tf
+++ Terraform/Skill/main.tf
@@ -1,4 +1,12 @@
 terraform {
+cloud { 
+    
+    organization = "Cx_As_Code" 
+
+    workspaces { 
+      name = "CI_CD" 
+    } 
+  } 
   required_providers {
     genesyscloud = {
       source  = "MyPureCloud/genesyscloud"
