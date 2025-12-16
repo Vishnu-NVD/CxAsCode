eba2e47b2881e21197cf37cd0a7e979796c35f77
Vishnu-NVD <vishnu.muthusamy@servion.com>
2025-12-16 15:40:17 +0530

    Update kb.tf

--- Terraform/Knowledge/kb.tf
+++ Terraform/Knowledge/kb.tf
@@ -1,5 +1,5 @@
 resource "genesyscloud_knowledge_knowledgebase" "example_knowledgebase" {
-  name          = "MyKB_via_CxAsCode_7"
+  name          = "MyKB_via_CxAsCode_8"
   description   = "An example knowledge base created via CI/CD"
   core_language = "en-US"
 }
