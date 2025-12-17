3f0155ad682b74c9b7ae41758879857c3eb0c6ec
Vishnu-NVD <vishnu.muthusamy@servion.com>
2025-12-17 12:31:08 +0530

    [destroy]

--- Terraform/Knowledge/kb.tf
+++ Terraform/Knowledge/kb.tf
@@ -1,5 +1,3 @@
 resource "genesyscloud_knowledge_knowledgebase" "example_knowledgebase" {
-  name          = "MyKB_via_CxAsCode_7"
-  description   = "An example knowledge base created via CI/CD"
-  core_language = "en-US"
+  
 }
