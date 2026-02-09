be574f92ed7028ceed981e59f2f288356faa0ee9
Vishnu-NVD <vishnu.muthusamy@servion.com>
2026-02-09 17:36:03 +0530

    Create dt_row.tf

---
+++ Terraform/Data Table/dt_row.tf
@@ -0,0 +1,18 @@
+resource "genesyscloud_architect_datatable_row" "john" {
+  datatable_id = genesyscloud_architect_datatable.customers_2.id
+  key_value    = "johnsmith@servion.com"
+  properties_json = jsonencode({
+    "id" = 2749
+    "First"    = "John"
+    "Last"        = "Smith"
+  })
+}
+resource "genesyscloud_architect_datatable_row" "vishnu" {
+  datatable_id = genesyscloud_architect_datatable.customers_1.id
+  key_value    = "vishnu.m@servion.com"
+  properties_json = jsonencode({
+    "id" = 2750
+    "First"    = "Vishnu"
+    "Last"        = "M"
+  })
+}
