16f163f689571a0bab4c7bd29cdcf023a80f9600
Vishnu-NVD <vishnu.muthusamy@servion.com>
2026-02-09 17:40:04 +0530

    Update queue.tf

--- Terraform/Queue/queue.tf
+++ Terraform/Queue/queue.tf
@@ -1,5 +1,5 @@
-resource "genesyscloud_routing_queue" "Queue" {
-  name = "Example Queue_CICD_demo_1"
+resource "genesyscloud_routing_queue" "Queue_2" {
+  name = "Example Queue_CICD_demo_2"
   media_settings_callback {
     enable_auto_answer        = false
     mode                      = "AgentFirst"
