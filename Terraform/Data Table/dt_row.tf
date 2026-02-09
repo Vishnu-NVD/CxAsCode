34def88d2d7471ad6afac397e50f9656a2280e0f
Vishnu-NVD <vishnu.muthusamy@servion.com>
2026-02-09 10:59:55 +0530

    Update dt_row.tf

--- Terraform/Data Table/dt_row.tf
+++ Terraform/Data Table/dt_row.tf
@@ -1,18 +1,21 @@
-resource "genesyscloud_architect_datatable_row" "dt_row" {
-  datatable_id = 47aadac4-377e-4506-921c-f1a562874cb7
-  "entities": [
- {
-  "key"    = "johnsmith@example.com"
-  "id" = 2749
-  "First"    = "John"
-  "Last"        = "Smith"
-  },
-   {
-   "key"    = "vishnu.m@servion.com"
-   "id" = 2750
-   "First"    = "Vishnu"
-   "Last"        = "M"
+resource "genesyscloud_architect_datatable_row" "john" {
+  datatable_id = "47aadac4-377e-4506-921c-f1a562874cb7"
+  key          = "johnsmith@example.com"
+
+  properties = {
+    id    = "2749"
+    First = "John"
+    Last  = "Smith"
+  }
 }
-]
+
+resource "genesyscloud_architect_datatable_row" "vishnu" {
+  datatable_id = "47aadac4-377e-4506-921c-f1a562874cb7"
+  key          = "vishnu.m@servion.com"
+
+  properties = {
+    id    = "2750"
+    First = "Vishnu"
+    Last  = "M"
+  }
 }
-  
