0e78a66cbf330729d78ec3ed1c0e0555c9c0010a
Vishnu-NVD <vishnu.muthusamy@servion.com>
2026-02-09 12:32:51 +0530

    Create Arthur.tf

---
+++ Terraform/User/Arthur.tf
@@ -0,0 +1,58 @@
+resource "genesyscloud_user" "Arthur" {
+  email           = "Arthur.root@servion.com"
+  name            = "Arthur Root"
+  state           = "active"
+  department      = "Development"
+  title           = "Senior Director"
+  acd_auto_answer = true
+  profile_skills  = ["Java", "Go"]
+  certifications  = ["Certified Developer"]
+  addresses {
+    other_emails {
+      address = "arthur@gmail.com"
+      type    = "HOME"
+    }
+    phone_numbers {
+      number     = "+13174181234"
+      media_type = "PHONE"
+      type       = "MOBILE"
+    }
+  }
+  
+  locations {
+    location_id = "45a09a4d-6d1f-4ead-b369-eb2e67533125"
+    notes       = "Office 201"
+  }
+  employer_info {
+    official_name = "Arthur Root"
+    employee_id   = "12345"
+    employee_type = "Full-time"
+    date_hire     = "2021-03-18"
+  }
+  routing_utilization {
+    call {
+      maximum_capacity = 1
+      include_non_acd  = true
+    }
+    callback {
+      maximum_capacity          = 2
+      include_non_acd           = false
+      interruptible_media_types = ["call", "email"]
+    }
+    chat {
+      maximum_capacity          = 3
+      include_non_acd           = false
+      interruptible_media_types = ["call"]
+    }
+    email {
+      maximum_capacity          = 2
+      include_non_acd           = false
+      interruptible_media_types = ["call", "chat"]
+    }
+    message {
+      maximum_capacity          = 4
+      include_non_acd           = false
+      interruptible_media_types = ["call", "chat"]
+    }
+  }
+}
